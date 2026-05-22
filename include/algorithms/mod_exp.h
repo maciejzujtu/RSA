ef890fd24689ba14a8c51de6916ed1533fbe6ee3
Maciej <maciej.nowak000@icloud.com>
2026-05-22 23:58:07 +0200

    Initial commit

---
+++ include/algorithms/mod_exp.h
@@ -0,0 +1,32 @@
+#ifndef RSA_MOD_EXP_H
+#define RSA_MOD_EXP_H
+
+#include <gmp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file miller_rabin.h
+ * @brief Miller-Rabin probabilistic primality test.
+ *
+ * FIPS 186-5 §B.3.1
+ * Implementation of Miller-Rabin's primality test as defined
+ * in FIPS 186-5 Appendix B.3.1. 
+ * approved DRBG (SP 800-90A).
+ *
+ * Error probability per call: <= 4^(-rounds).
+ * FIPS minimum rounds: 5 for 2048-bit, 4 for 3072-bit and above.
+ */
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+#endif
